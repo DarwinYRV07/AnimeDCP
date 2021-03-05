1d4f375ab8b95594ffe1a9f729ba290db5f9e5e6
Carlos-Aguilar7 <carlosaguilar07armas7@gmail.com>
2021-03-05 16:00:44 -0600

    Diseño de pantalla ChangePwdScreen

--- src/components/screen/changePwdScreen.js
+++ src/components/screen/changePwdScreen.js
@@ -1,13 +1,21 @@
 import React from "react"
-import {View, StyleSheet,Text} from "react-native"
+import { View, StyleSheet, Text, TextInput, Dimensions } from "react-native"
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Button from "../button/Button";
 
-const tab = createBottomTabNavigator();
+const {width} = Dimensions.get("screen");
 
 const changePwdScreen = () =>{
     return(
         <View style={styles.container}>
-            <Text>changePwdScreen screen</Text>
+            <Text style={styles.title}>Change password</Text>
+            <Text style={styles.text}>Current password</Text>
+            <TextInput style={styles.input} placeholder="Enter current password"/>
+            <Text style={styles.text}>New password</Text>
+            <TextInput style={styles.input} placeholder="Enter new password"/>
+            <Text style={styles.text}>Confirm password</Text>
+            <TextInput style={styles.input} placeholder="Confirm password"/>
+            <Button title="SAVE"/>
         </View>
     )
 }
@@ -16,9 +24,37 @@ const changePwdScreen = () =>{
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#252c4a',
+      backgroundColor: '#2F353A',
       alignItems: 'center',
       justifyContent: 'center',
+    },
+    text: {
+      color:'#22DEFA',
+      marginLeft: 15,
+      marginTop:10,
+      marginBottom:10,
+      fontSize:20,
+      textAlign:'left',
+      width: width*0.9
+    },
+    input: {
+      backgroundColor:'#FFF',
+      color:'#000',
+      textAlign:'left',
+      paddingLeft: 10,
+      justifyContent:'center',
+      borderRadius: 15,
+      width: width*0.9,
+      height: 40,
+      marginBottom: 20
+    },
+    title: {
+      alignSelf:'center',
+      alignContent:'center',
+      padding:15,
+      marginBottom:10,
+      fontSize:28,
+      color:'#FFF'
     },
     
   });
