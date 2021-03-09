c0bc74d9f55ac488ee7d3b5644c6ff9971333358
Carlos-Aguilar7 <carlosaguilar07armas7@gmail.com>
2021-03-08 22:36:10 -0600

    Creacion de cardV y agregación a la pantalla myListScreen

---
+++ src/components/cardV/CardV.js
@@ -0,0 +1,69 @@
+import React from "react"
+import {View, StyleSheet,Text,Dimensions} from "react-native"
+import {Image,Button,Icon} from 'react-native-elements'
+
+
+const{width,height}=Dimensions.get("screen");
+
+const CardV = () =>{
+    return(
+      <View style={styles.cardRow}>
+        <Image style={styles.portada} source={{uri: require("../../assets/imgPrueba.jpg") }}/>
+        <View >
+          <Text style={styles.titulo}>SHINGEKI NO KYOJIN</Text>
+          <Text style={styles.fecha}>Publicado: 15 de abril de 2013</Text>
+        </View>
+        <Button
+            icon={
+              <Icon
+                name="delete"
+                size={20}
+                color="white"
+              />
+            }
+            iconTop
+            type="clear"
+          />
+      </View>
+      
+    )
+}
+
+
+const styles = StyleSheet.create({
+    portada:{
+      marginBotton:5,
+      width:60,
+      height:60,
+      resizeMode:"contain"
+    },
+    titulo:{
+      fontSize:20,
+      color:"#ffff",
+      alignSelf:"stretch"
+    },
+    fecha:{
+      fontSize:12,
+      color:"#ff3f"
+    },cardRow:{
+        borderRadius:10,
+        marginTop:10,
+        backgroundColor: '#413F4F',
+        alignItems: 'center',
+        justifyContent:'center',
+        flexDirection:"row",
+        shadowColor: "#000",
+        shadowOffset: {
+        width: 0,
+        height: 6,
+      },
+      shadowOpacity: 0.39,
+      shadowRadius: 8.30,
+      width:width*0.9,
+      height:height*0.1,
+      elevation: 13,
+    }
+    
+  });
+
+export default CardV;
