1a9e4eb2018d17b72b53e5fd14167a66b45aed7c
DarwinYRV07 <darwinventura840@gnail.com>
2021-04-07 23:55:21 -0600

    Modificacion de animeScreen (diseño)

--- src/components/screen/animeScreen.js
+++ src/components/screen/animeScreen.js
@@ -5,6 +5,7 @@ import Score from "../shared/Score";
 import Gender from "../shared/Gender";
 import Type from "../shared/Type";
 import Airing from "../shared/Airing";
+import Caratula from "../shared/Caratula";
 import {fetchAnimeEs, fetchAnimeGenero } from "../../api/index";
 //import Video from "react-native-video";
 
@@ -55,85 +56,89 @@ const animeScreen =(navigate)=>{
     },[]);
 
    //console.log(animeInfo.related.Sequel);
-   console.log(animeInfo.trailer_url);
+   console.log(animeInfo.image_url);
+   const caratu = animeInfo.image_url;
+   console.log("AAAAAA"+caratu);
    //console.log(relacionado[0].mal_id);
 
     return(
         <ScrollView>
             <View style={styles.container}>
-            
-                {/*<View style={{backgroundColor:"gray", width:width * 100, height: height * 0.25,}}>
-                <Text>hOL</Text>
-                </View>*/}
-                <View style={styles.ContenedorTituloImg}>
-                    <Image source={{uri: animeInfo.image_url }} style={styles.imagen}/>
-                    <Text h5 style={styles.titlestyle}>{animeInfo.title}</Text>
-                </View>
-
-                <View style={styles.ItemShow}>
-                    <View style={styles.ItemGener}>
-                        <Score type={animeInfo.score} title={animeInfo.score}/>
-                        <Type type={animeInfo.type} title={animeInfo.type}/>              
-                        <Airing type={animeInfo.airing}/>
-                    </View>
-
-                    <View style={styles.ItemGener}>
-                        {genero!=undefined?(<FlatList
-                            ListEmptyComponent={<Text>No tiene Generos!!</Text>}
-                            data={genero}
-                            key={({item}) => item}
-                            horizontal={true}
-                            renderItem={({item}) => {
-                            return (
-                                <View>
-                                    <Gender title= {item} type={item}/>
-                                </View>
-                            )   
-                            }}                     
-                        />):(null)}
-                    </View>
+                <View style={{marginBottom:30,marginTop:20}}>
+                    {/*<View style={{backgroundColor:"gray", width:width * 100, height: height * 0.25,}}>
+                    <Text>hOL</Text>
+                    </View>*/}
+                    <View style={styles.ContenedorTituloImg}>
                         
-                
-                    <View style={styles.ItemCap}>
-                        <Text>Total Capitulos: {animeInfo.episodes} </Text>
-                        <Text>Duration: {animeInfo.duration} </Text>
+                        <Image source={{uri:caratu}} style={styles.imagen} />
+                        <Text h5 style={styles.titlestyle}>{animeInfo.title}</Text>
                     </View>
+
+                    <View style={styles.ItemShow}>
+                        <View style={styles.ItemGener}>
+                            <Score type={animeInfo.score} title={animeInfo.score}/>
+                            <Type type={animeInfo.type} title={animeInfo.type}/>              
+                            <Airing type={animeInfo.airing}/>
+                        </View>
+
+                        <View style={styles.ItemGener}>
+                            {genero!=undefined?(<FlatList
+                                ListEmptyComponent={<Text>No tiene Generos!!</Text>}
+                                data={genero}
+                                key={({item}) => item}
+                                horizontal={true}
+                                renderItem={({item}) => {
+                                return (
+                                    <View>
+                                        <Gender title= {item} type={item}/>
+                                    </View>
+                                )   
+                                }}                     
+                            />):(null)}
+                        </View>
+                            
                     
-                </View>
-                <View style={styles.ItemShow}>
-                    <View style={styles.ItemDescription}>
-                        <Text>Descripcion</Text>
-                        <Text>{animeInfo.synopsis}</Text>
-                    </View>
-                </View>
-
-                <View style={styles.ItemShow}>
-                    <Text style={styles.text}>Trailer!</Text>
-                    <View style={styles.ItemTrailer}>
-                        {/*<Video 
-                            source={{uri:animeInfo.trailer_url}}
-                            style={{position: 'absolute',
-                            top: 0,
-                            left: 0,
-                            bottom: 0,
-                            right: 0,
-                        }}
-                        />*/}
+                        <View style={styles.ItemCap}>
+                            <Text>Chapters: {animeInfo.episodes} </Text>
+                            <Text>Duration: {animeInfo.duration} </Text>
+                        </View>
                         
-                       {/* {relacionado!=undefined?(<FlatList
-                            ListEmptyComponent={<Text>No ahi continuacion del Anime!! </Text>}
-                            data={relacionado}
-                            key={({item}) => item.mal_id}
-                            horizontal={true}
-                            renderItem={({item}) => {
-                            return (
-                                <View>
-                                    <Text>{item.name}</Text>
-                                    <Type type={item.type} title={item.type}/>  
-                                </View>
-                            )   
-                            }}   
-                        />):(null)}*/}
+                    </View>
+                    <View style={styles.ItemShow}>
+                        <View style={styles.ItemDescription}>
+                            <Text>Descripcion</Text>
+                            <Text>{animeInfo.synopsis}</Text>
+                        </View>
+                    </View>
+
+                    <View style={styles.ItemShow}>
+                        <Text style={styles.text}>Trailer!</Text>
+                        <View style={styles.ItemTrailer}>
+                            {/*<Video 
+                                source={{uri:animeInfo.trailer_url}}
+                                style={{position: 'absolute',
+                                top: 0,
+                                left: 0,
+                                bottom: 0,
+                                right: 0,
+                            }}
+                            />*/}
+                            
+                        {/* {relacionado!=undefined?(<FlatList
+                                ListEmptyComponent={<Text>No ahi continuacion del Anime!! </Text>}
+                                data={relacionado}
+                                key={({item}) => item.mal_id}
+                                horizontal={true}
+                                renderItem={({item}) => {
+                                return (
+                                    <View>
+                                        <Text>{item.name}</Text>
+                                        <Type type={item.type} title={item.type}/>  
+                                    </View>
+                                )   
+                                }}   
+                            />):(null)}*/}
+                        </View>
                     </View>
                 </View>
             </View>
@@ -147,15 +152,17 @@ const styles = StyleSheet.create({
         backgroundColor: '#2F353A',
         alignItems: 'center',
         justifyContent: 'flex-start', 
-        height: height * 0.100
+        height: height * 0.100,
 
       },
       titlestyle:{
           textAlign:"center",
           marginTop:10,
-          fontSize:16,
+          marginBottom:5,
+          fontSize:18,
           padding:3,
-          color:"#fff"
+          color:"#22DEFA",
+          fontWeight:"bold"
       },
     text: {
         //color:'#fff',
@@ -202,26 +209,26 @@ const styles = StyleSheet.create({
           marginTop:5,
           borderRadius:12,
           shadowColor: '#000',
-          shadowOffset: { width: 1, height: 5 },
+          shadowOffset: { width: 2, height: 5 },
           shadowOpacity: 0.7,
           shadowRadius: 10,
           elevation: 3,
           margin:1,
-          backgroundColor:"#fff",
+          backgroundColor:"#BFC9CE",
           width: width * 0.90,
           paddingBottom:10
-      },
-      imagen:{
-          width:120,
-          height:150,
-          marginTop:20,
-          //marginTop:-30,
-          borderRadius:15,
-          //position:"absolute"
       },
       ItemTrailer:{
           padding:10,
       },
+      imagen:{
+        width:120,
+        height:150,
+        marginTop:20,
+        //marginTop:-30,
+        borderRadius:15,
+        //position:"absolute"
+    },
 
 })
 
