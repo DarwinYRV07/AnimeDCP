376f35893715ed731afc98e3d1768c4a3d49a613
Carlos-Aguilar7 <carlosaguilar07armas7@gmail.com>
2021-04-10 01:00:24 -0600

    Pantalla en proceso.

--- src/components/screen/myListScreen.js
+++ src/components/screen/myListScreen.js
@@ -7,71 +7,11 @@ import CardV from '../cards/CardV'
 const{width,height}=Dimensions.get("screen");
 
 const myListScreen =()=>{
-    const [animename, setAnimeName] = useState("");
-    const [animeNameError, setAnimeNameError] = useState(false);
-    const [data, setData] = useState([]);
-    const [search, setSearch] = useState(false);
-
-    useEffect(() => {
-        handleSearchAnimes();
-        console.log(data);
-        console.log(animename);
-    }, [search])
-    
-    const handleSearchAnimes = async ()=>{
-        const animeList = await fetchAnimeSearch(animename);
-        setData(animeList);
-    }
-
-    const handleVerify = (input) => {
-        if (input === "animename") {
-          if (!animename) setAnimeNameError(true);
-          else setAnimeNameError(false);
-        }
-    };
-
     return(
         <View style={styles.container}>
             <Text style={styles.text}>
-                Find your favorite anime.
+                My list screen
             </Text>
-            <View style={styles.searchbar}>
-                <TextInput
-                    style={styles.input} 
-                    placeholder={"Enter anime name"}
-                    value={animename}
-                    onChangeText={setAnimeName}
-                    onBlur={() => {handleVerify("animename");}}errorMessage={ animeNameError? "Please enter the name": null}>
-                </TextInput>
-                <Button title="Search" onPress={()=>{
-                    setData([]);
-                    setSearch(!search);
-                    console.log(search);
-                }}/>
-            </View>
-            <View style={styles.container}>   
-                {data!=undefined?(<FlatList
-                            ListEmptyComponent={<Text>No hay animes disponibles!</Text>}
-                            data={data}
-                            key={({item}) => item.mal_id}
-                            horizontal={false}
-                            renderItem={({item}) => {
-                            return (
-                                <View>
-                                     <CardV
-                                        url={item.image_url}
-                                        name={item.title}
-                                        id={item.mal_id}
-                                        punt={item.score}
-                                        date={item.start_date}
-                                        btn={false}
-                                     />
-                                </View>
-                            )   
-                            }}
-                            
-                />):(null)}
-            </View>
         </View>
         
     )
@@ -83,14 +23,6 @@ const styles = StyleSheet.create({
         backgroundColor: '#2F353A',
         alignItems: 'center',
         justifyContent: 'flex-start',
-        marginTop: 25
-    },
-    searchbar: {
-        borderRadius:5,
-        marginTop:10,
-        alignItems:"center",
-        justifyContent:"center",
-        flexDirection:"row"
     },
     text: {
         color:'#22DEFA',
@@ -102,19 +34,7 @@ const styles = StyleSheet.create({
         width: width*0.9,
         marginTop: 40,
         marginBottom: 10
-      },
-      input:{
-        backgroundColor:"#fff",
-        color:"#000",
-        textAlign:"left",
-        paddingLeft:10,
-        justifyContent:"center",
-        borderRadius:15,
-        width: width*0.7,
-        height: 40,
-        marginBottom:5,
-        marginRight: 20
-    }
+      }
 })
 
 export default myListScreen;
