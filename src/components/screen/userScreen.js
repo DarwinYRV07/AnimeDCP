8eac01494f648cbd6b75f896a08243036f69539d
Carlos-Aguilar7 <carlosaguilar07armas7@gmail.com>
2021-04-11 17:01:59 -0600

    Modificación del diseño de la pantalla userScreen.

--- src/components/screen/userScreen.js
+++ src/components/screen/userScreen.js
@@ -1,6 +1,9 @@
 import React,{useState,useEffect,useContext} from 'react' 
 import { Dimensions} from 'react-native'
-import {StyleSheet, Text, View,ImageBackground,Switch} from 'react-native'
+import {StyleSheet, Text, View,ImageBackground,Switch, TouchableOpacity} from 'react-native'
+import { Avatar, ListItem } from 'react-native-elements'
+import Icon from "react-native-vector-icons/FontAwesome5";
+import {Divider} from 'react-native-paper'
 import {firebase} from '../../Firebase'
 import Button from '../button/button'
 import theme from "../../Theme";
@@ -40,24 +43,58 @@ const userScreen =({navigation})=>{
     
     return(
         <View style={styles.container}>
-          <ImageBackground source={require("../../../assets/background.jpg")} style={styles.image}> 
+            <View style={styles.header}><Text style={styles.textHeader}>SETTINGS</Text></View>
+            <Avatar
+                rounded
+                size='xlarge'
+                source={ require('../../../assets/animeDCPIcon.png') }
+                />
+            <ListItem containerStyle={styles.listItem} style={{marginTop:20}}>
+                <ListItem.Content style={styles.listContent}>
+                <ListItem.Title style={styles.titulo}>{"Current User"}</ListItem.Title>
+                <ListItem.Subtitle style={styles.subtitulo}>{"Pruebas"}</ListItem.Subtitle>
+                </ListItem.Content>
+            </ListItem>
+            <Divider style={styles.divider}/>
+            <ListItem containerStyle={styles.listItem}>
+                <ListItem.Content style={styles.listContent}>
+                <ListItem.Title style={styles.titulo}>{"Correo"}</ListItem.Title>
+                <ListItem.Subtitle style={styles.subtitulo}>{user.email}</ListItem.Subtitle>
+                </ListItem.Content>
+            </ListItem>
+            <Divider style={styles.divider}/>
+            <ListItem containerStyle={styles.listItem}>
+                <ListItem.Content style={styles.listContent}>
+                <TouchableOpacity onPress={()=>{navigation.navigate("ChangePwd")}}>
+                    <View style={{flexDirection:"row"}}>
+                      <Text>
+                        <Text style={styles.titulo}>Change Password</Text>
+                      </Text>
+                      <Text style={{marginLeft:width*0.47}}>
+                        {<Icon style={{fontSize:20,}} name={'sync-alt'}/>}
+                      </Text>
+                    </View>
+                </TouchableOpacity>
+                </ListItem.Content>
+            </ListItem>
+            <Divider style={styles.divider}/>
+            <ListItem containerStyle={styles.listItem}>
+                <ListItem.Content style={styles.listContent}>
+                <TouchableOpacity onPress={handleLogOut}>
+                    <View style={{flexDirection:"row"}}>
+                      <Text>
+                        <Text style={styles.titulo}>LogOut</Text>
+                      </Text>
+                      <Text style={{marginLeft:width*0.71}}>
+                        {<Icon style={{fontSize:23}} name={'sign-out-alt'}/>}
+                      </Text>
+                    </View>
+                </TouchableOpacity>
+                </ListItem.Content>
+            </ListItem>
                 {/* <View style={styles.header}>
                     <Text backgroundColor={colorHeader}>SETTINGS</Text>
                 </View> */}
-                {texto()}
-                <Text style={styles.text}>Current User:</Text>
-                <Text style={styles.text}>{"PROBANDO"}</Text>
-                <Switch
-                    trackColor={{ false: "#767577", true: "#81b0ff" }}
-                    thumbColor={true ? "#f5dd4b" : "#f4f3f4"}
-                    ios_backgroundColor="#3e3e3e"
-                    //onValueChange={setDarkMode}
-                    value={true}
-                />
-                <Button title="logOut" 
-                callback={handleLogOut}/>
-                <Button title="Change Password" callback={()=>{navigation.navigate("ChangePwd")}}/>
-                </ImageBackground>
         </View>
     )
 }
@@ -66,16 +103,43 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#2F353A',
-        //justifyContent: 'center',
-        //alignItems:"center"
+        justifyContent: 'center',
+        alignItems:"center"
+      },
+      listItem:{
+        width:width*1,
+        backgroundColor: '#BFC9CE',
+        height:height*0.09
+      },
+      listContent:{
+      },
+      titulo:{
+        fontSize:20,
+        flexDirection:"row"
+      },
+      subtitulo:{
+        fontSize:15
       },
       text:{
           color:"white",
           fontSize:20,
-          marginBottom:15,
+      },
+      textCorreo:{
+        color:"#FFF",
+        fontSize:15,
+      },
+      divider:{
+        width:width*0.9,
+        height:2,
+      },
+      textHeader:{
+        color:"#BFC9CE",
+        fontSize:30,
+        marginTop:10,
+        marginBottom:15,
+        position:"relative"
       },
       header:{
-        marginTop:45,
         marginBottom:15,
         position:"relative",
         borderRadius:5,
@@ -96,6 +160,22 @@ const styles = StyleSheet.create({
         width:width*1,
         alignItems:"center"
       },
+      header:{
+        marginTop:20,
+        marginBottom:20,
+        position:"relative",
+        borderRadius:5,
+        backgroundColor: '#2F353A',
+        alignItems:"center",
+        justifyContent:"center",
+        flexDirection:"row",
+        shadowColor: "#000",
+        width:width*0.95,
+        shadowOffset: {
+            width: 0,
+            height: 4,
+        }
+    },
 })
 
 export default userScreen;
