f35eabae66cc02f6db1cb0c9f9a65e6a36f97a70
EmilPaizano <pabloemilio.beta@gmail.com>
2021-04-12 00:46:11 -0600

    Solucion de error al importar Image

--- src/components/cards/CardX.js
+++ src/components/cards/CardX.js
@@ -1,6 +1,6 @@
 import React from "react"
 import {View, StyleSheet,Text,Dimensions, TouchableOpacity } from "react-native"
-import {Button} from 'react-native-elements'
+import {Image,Button} from 'react-native-elements'
 import Icon from "react-native-vector-icons/FontAwesome5";
 
 
